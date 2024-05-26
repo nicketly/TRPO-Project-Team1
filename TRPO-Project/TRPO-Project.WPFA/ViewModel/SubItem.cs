36877ffcde4c014d9a38074b4066b39745a81581
Diana <Diana.fedotova2002@mail.ru>
2024-05-27 01:02:22 +0500

    27.05
    разработка элементов View

---
+++ TRPO-Project/TRPO-Project.WPFA/ViewModel/SubItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+
+namespace TRPO_Project.WPFA.ViewModel
+{
+    public class SubItem
+    {
+        public SubItem(PackIconKind iconSub, string name, string check, string changes, UserControl screen = null)
+        {
+            IconSub = iconSub;
+            Name = name;
+            Check = check;
+            Changes = changes;
+            Screen = screen;
+        }
+
+        public SubItem(string name, string check, UserControl screen = null)
+        {
+            Name = name;
+            Check = check;
+            Screen = screen;
+        }
+
+        public PackIconKind IconSub { get; private set; }
+        public string Name { get; private set; }
+        public string Check { get; private set; }
+        public string Changes { get; private set; }
+        public UserControl Screen { get; private set; }
+    }
+}
