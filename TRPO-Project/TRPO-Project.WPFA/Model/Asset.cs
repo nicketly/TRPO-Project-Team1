9fdca1708cb54bf61ab5c4b39d611fcf7fb00f3f
nicketly <dleinetu2016@gmail.com>
2024-06-05 21:16:43 +0500

    Настройка баланса, стоимости портфеля

---
+++ TRPO-Project/TRPO-Project.WPFA/Model/Asset.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO_Project.WPFA.Model
+{
+    public class Asset
+    {
+        public string Код { get; set; }
+        public string Наименование { get; set; }
+        public string Тип { get; set; }
+    }
+}
---
+++ TRPO-Project/TRPO-Project.WPFA/ViewModel/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO_Project.WPFA.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private decimal _balanceValue;
+        private decimal _briefcaseValue;
+        public decimal BalanceValue
+        {
+            get { return _balanceValue; }
+            set
+            {
+                if (_balanceValue != value)
+                {
+                    _balanceValue = value;
+                    OnPropertyChanged(nameof(BalanceValue));
+                }
+            }
+        }
+
+        public decimal BriefcaseValue
+        {
+            get { return _briefcaseValue; }
+            set
+            {
+                if (_briefcaseValue != value)
+                {
+                    _briefcaseValue = value;
+                    OnPropertyChanged(nameof(BriefcaseValue));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
