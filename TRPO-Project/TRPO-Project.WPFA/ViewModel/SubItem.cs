b08673ec806f2f8e579621cbfdedfe49c036cb9b
nicketly <dleinetu2016@gmail.com>
2024-06-07 23:35:37 +0500

    125

--- TRPO-Project/TRPO-Project.WPFA/ViewModel/SubItem.cs
+++ TRPO-Project/TRPO-Project.WPFA/ViewModel/SubItem.cs
@@ -5,7 +5,7 @@ namespace TRPO_Project.WPFA.ViewModel
 {
     public class SubItem
     {
-        public SubItem(PackIconKind iconSub, string name, decimal check, string changes, UserControl screen = null)
+        public SubItem(PackIconKind iconSub, string name, decimal check, double changes, UserControl screen = null)
         {
             IconSub = iconSub;
             Name = name;
@@ -24,7 +24,7 @@ namespace TRPO_Project.WPFA.ViewModel
         public PackIconKind IconSub { get; private set; }
         public string Name { get; private set; }
         public decimal Check { get; private set; }
-        public string Changes { get; private set; }
+        public double Changes { get; private set; }
         public UserControl Screen { get; private set; }
     }
 }
